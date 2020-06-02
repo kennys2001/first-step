6708eab41ed20e54409fa480643e69d09578c604
“kennys2001” <1851344171@qq.com>
2020-06-02 14:06:29 +0800

    first ss

---
+++ step 0/step 0/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  step 0
+//
+//  Created by lyz2333 on 2020/6/2.
+//  Copyright © 2020 lyz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n"<<std::endl<<"1";
+    
+    return 0;
+}
